9a83049e34f7598e9ecc2417b3b8427070068ab8
dev.parkjk9425 <dev.parkjk9425@gmail.com>
2025-05-07 16:47:55 +0900

    no message

--- server/src/main/resources/sql/tbl_channel_post_file.sql
+++ server/src/main/resources/sql/tbl_channel_post_file.sql
@@ -1,12 +1,11 @@
-
 -- 게시물 파일 테이블
-create table tbl_channel_post_file (
-                                       id 		            bigint primary key,
-                                       post_id 		        bigint not null,
-                                       constraint fk_channel_post_file_channel_post foreign key (id)
-                                           references tbl_file (id),
-                                       constraint fk_channel_post_file_post foreign key (post_id)
-                                           references tbl_channel_post (id)
+create table tbl_channel_post_file
+(
+    id      bigint primary key,
+    post_id bigint not null,
+    constraint fk_channel_post_file_channel_post foreign key (id)
+        references tbl_file (id),
+    constraint fk_channel_post_file_post foreign key (post_id)
+        references tbl_channel_post (id)
 );
-
 
--- server/src/main/resources/sql/tbl_community_post_like.sql
+++ server/src/main/resources/sql/tbl_community_post_like.sql
@@ -1,14 +1,16 @@
 -- community 좋아요 테이블
-create table tbl_community_post_like (
-    id 		            bigint primary key,
-    member_id 	        bigint not null,
-    post_id 	    bigint not null,
+create table tbl_community_post_like
+(
+    id        bigint primary key,
+    member_id bigint not null,
+    post_id   bigint not null,
     constraint fk_community_post_like_like foreign key (id)
-    references tbl_like (id) ,
+        references tbl_like (id),
     constraint fk_community_post_like_member foreign key (member_id)
-    references tbl_member (id) ,
+        references tbl_member (id),
     constraint fk_community_post_like_community_post foreign key (post_id)
-    references tbl_community_post (id)
+        references tbl_community_post (id)
 );
 
 
+drop table tbl_community_post_like;
